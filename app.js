e7e83d2223c75b19228072ea8d5511a16eb81e16
Jason Botzas-Coluni <jason.botzas.coluni@gmail.com>
2020-01-25 13:14:25 -0500

    pushpush

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
 const express = require('express')
 require('dotenv').config()
+var admin = require("firebase-admin");
+
 
 const PORT =  process.env.PORT
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello World!'))
+let rockChannelTime = null
+let rapChannelTime = null
+let popChannelTime = null
+let jazzChannelTime = null
+let indieChannelTime = null
+
+
+let serviceAccount = require(`${__dirname}/firebase_auth.json`);
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://conuhacksradio.firebaseio.com"
+  })
+
+// var db = admin.database
+
+
+app.get('/', (req, res) => res.send("Hola!") && console.log(admin.database().ref('/modes/')))
 
 app.listen(PORT, () => console.log(`Lesssa get dis breaaaad on ${PORT}!!!!`))
